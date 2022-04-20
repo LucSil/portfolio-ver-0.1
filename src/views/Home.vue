3fc6ba1def159049c5b8c6020ba48645baffd8ab
LucSil <luciansilva351@gmail.com>
2022-04-20 10:48:48 +0100

    imported Social into Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,29 +12,15 @@
       </h1>
       <p class="text-center font-roboto text-30%">Front-end Developer</p>
     </div>
-    <ul id="social" class="grid place-items-center mt-8">
-      <li>
-        <a href="https://github.com/LucSil" target="blank">
-          <ion-icon class="socialLinks" name="logo-github"></ion-icon>
-        </a>
-      </li>
-      <li>
-        <a href="https://twitter.com/Luc_Sil_" target="blank">
-          <ion-icon class="socialLinks" name="logo-twitter"></ion-icon
-        ></a>
-      </li>
-      <li>
-        <a href="https://wa.me/%2B2349057666676" target="blank">
-          <ion-icon class="socialLinks" name="logo-whatsapp"></ion-icon
-        ></a>
-      </li>
-    </ul>
+    <Social />
   </section>
 </template>
 
 <script>
+import Social from "../components/Social.vue";
 export default {
   name: "Home",
+  components: { Social },
 };
 </script>
 
