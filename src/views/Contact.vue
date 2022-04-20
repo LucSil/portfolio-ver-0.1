94032cd7911ccd980dbda79595da7838e3da9e2b
LucSil <luciansilva351@gmail.com>
2022-04-20 10:41:31 +0100

    templating for Contact.vue complete

---
+++ src/views/Contact.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <div>
+      <h1>Get In Touch</h1>
+      <p></p>
+    </div>
+    <button>
+      <a
+        href="mailto:chijiokeobioha351@gmail.com"
+        rel="noopener noreferrer"
+        target="_blank"
+      ></a>
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+};
+</script>
