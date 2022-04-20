ea5505c21b24252b4fa1e2e35122df0558602a00
LucSil <luciansilva351@gmail.com>
2022-04-20 11:19:26 +0100

    initial styling for Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,19 @@
 <template>
-  <section>
+  <section class="grid place-items-center mt-20">
     <div>
-      <h1>Get In Touch</h1>
+      <h1 class="font-raleway text-10-a text-4xl">Get In Touch</h1>
       <p></p>
     </div>
-    <button>
+    <button
+      class="border-2 border-10-b rounded-tl-xl rounded-br-xl px-8 py-4 hover:bg-[#a4b49462] transition duration-200 ease-in-out"
+    >
       <a
+        class="font-roboto font-medium text-10-b tracking-wider"
         href="mailto:chijiokeobioha351@gmail.com"
         rel="noopener noreferrer"
         target="_blank"
-      ></a>
+        >Say Hello</a
+      >
     </button>
   </section>
 </template>
