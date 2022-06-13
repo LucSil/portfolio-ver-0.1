47a6ee93eb881d9db659995c27d9db7f55f1f021
LucSil <luciansilva351@gmail.com>
2022-06-13 20:58:47 +0100

    lazy-loaded routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-import Work from "../views/Work.vue";
-import Contact from "../views/Contact.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: () => import("@/views/Home.vue"),
   },
   {
     path: "/about",
     name: "About",
-    component: About,
+    component: () => import("@/views/About.vue"),
   },
   {
     path: "/work",
     name: "Work",
-    component: Work,
+    component: () => import("@/views/Work.vue"),
   },
   {
     path: "/contact",
     name: "Contact",
-    component: Contact,
+    component: () => import("@/views/Contact.vue"),
   },
 ];
 
