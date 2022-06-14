9ddcf45b8533aa5998360107c6b9ed2c05d2c83b
LucSil <luciansilva351@gmail.com>
2022-06-14 06:52:29 +0100

    additions to template and styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-  <footer>Designed & Built by <em>Destiny Obioha</em></footer>
+  <footer class="fixed bottom-0">
+    <p class="py-6 text-xs text-10-b opacity-60">
+      <span class="text-10-a">Designed & Built by</span> <em>Destiny Obioha</em>
+    </p>
+  </footer>
 </template>
 
 <script>
