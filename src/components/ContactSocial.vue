fec37812ff06eb13f21bcefcfc292c7571309da4
LucSil <luciansilva351@gmail.com>
2022-06-14 06:13:37 +0100

    created ContactSocial.vue; completed templating and styling

---
+++ src/components/ContactSocial.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul id="social">
+    <li>
+      <a href="https://github.com/LucSil" target="blank">
+        <ion-icon class="contact-social-links" name="logo-github"></ion-icon>
+      </a>
+    </li>
+    <li>
+      <a href="https://twitter.com/Luc_Sil_" target="blank">
+        <ion-icon class="contact-social-links" name="logo-twitter"></ion-icon
+      ></a>
+    </li>
+    <li>
+      <a href="https://wa.me/%2B2349057666676" target="blank">
+        <ion-icon class="contact-social-links" name="logo-whatsapp"></ion-icon
+      ></a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Social",
+};
+</script>
+
+<style></style>
