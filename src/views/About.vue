3a52e9e1b1167106b4bcf32f8f5ac900cb351754
LucSil <luciansilva351@gmail.com>
2022-06-15 10:56:28 +0100

    added font styles

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,19 @@
 <template>
   <section class="mt-10 px-6 text-10-a">
     <!-- About Me -->
-    <p class="text-base leading-8">
+    <p class="text-base leading-8 font-raleway">
       Hello! My name is Destiny and I enjoy creating websites. I initially began
       with HTML & CSS, creating little web components, and was scared of trying
       out JavaScript. Later on, I learnt the
-      <em class="text-10-b">Art Of Logical Creativity</em> and embraced
-      <em class="text-10-b">Algorithmic Thinking</em>, which enabled me to
-      understand the concept of programming and learn JavaScript.
+      <em class="text-10-b font-roboto">Art Of Logical Creativity</em> and
+      embraced <em class="text-10-b font-roboto">Algorithmic Thinking</em>,
+      which enabled me to understand the concept of programming and learn
+      JavaScript.
     </p>
 
     <!-- Technology used -->
     <div class="text-base mt-10 mb-4">
-      <p class="mb-4">Here's the tech I make use of:</p>
+      <p class="mb-4 font-raleway">Here's the tech I make use of:</p>
       <section class="grid grid-cols-2 text-30%">
         <!-- Grid Col 1 -->
         <div class="grid tech-grid">
