81848160a61df0e807e64079b6b744279346226f
LucSil <luciansilva351@gmail.com>
2022-06-16 10:07:26 +0100

    templating and styling for overlapping gallery

---
+++ src/views/Work.vue
@@ -0,0 +1,92 @@
+<template>
+  <section
+    class="place-items-center h-screen w-full flex flex-col overflow-auto"
+  >
+    <!-- Image One -->
+    <div class="divA bg-[#e7e7e7]">
+      <!-- Img -->
+      <div class="divB">
+        <img class="object-fill" src="../assets/images/work/loopstudios.png" />
+
+        <!-- Links to Repository and Live Demo -->
+        <div class="link-div a">
+          <!-- Code-Repo -->
+          <a href="https://github.com/LucSil/loopstudios" target="blank">
+            <ion-icon class="repo-one" name="logo-github"></ion-icon>
+          </a>
+          <!-- Live-Demo -->
+          <a href="https://lucsil-loopstudios.netlify.app/" target="blank">
+            <ion-icon class="demo-one" name="open-outline"></ion-icon>
+          </a>
+        </div>
+      </div>
+      <!-- Link -->
+    </div>
+
+    <!-- Image Two -->
+    <div class="divA bg-[#293241]">
+      <!-- Img -->
+      <div class="divB">
+        <img src="../assets/images/work/room.png" />
+
+        <!-- Links to Repository and Live Demo -->
+        <div class="link-div b">
+          <!-- Code-Repo -->
+          <a href="https://github.com/LucSil/room" target="blank">
+            <ion-icon class="repo-two" name="logo-github"></ion-icon>
+          </a>
+          <!-- Live-Demo -->
+          <a href="https://lucsil-room.netlify.app/" target="blank">
+            <ion-icon class="demo-two" name="open-outline"></ion-icon>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Image Three -->
+    <div class="divA bg-[#000b0e]">
+      <!-- Img -->
+      <div class="divB">
+        <img src="../assets/images/work/sunnyside.png" />
+
+        <!-- Links to Repository and Live Demo -->
+        <div class="link-div c">
+          <!-- Code-Repo -->
+          <a href="https://github.com/LucSil/sunnyside" target="blank">
+            <ion-icon class="repo-three" name="logo-github"></ion-icon>
+          </a>
+          <!-- Live-Demo -->
+          <a href="https://lucsil-sunnyside.netlify.app/" target="blank">
+            <ion-icon class="demo-three" name="open-outline"></ion-icon>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Image Four -->
+    <div class="divA bg-[#e7e7e7]">
+      <!-- Img -->
+      <div class="divB">
+        <img src="../assets/images/work/advice.png" />
+
+        <!-- Links to Repository and Live Demo -->
+        <div class="link-div d">
+          <!-- Code-Repo -->
+          <a href="https://github.com/LucSil/advice-app" target="blank">
+            <ion-icon class="repo-four" name="logo-github"></ion-icon>
+          </a>
+          <!-- Live-Demo -->
+          <a href="https://lucsil-advice-app.netlify.app/" target="blank">
+            <ion-icon class="demo-four" name="open-outline"></ion-icon>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Work",
+};
+</script>
