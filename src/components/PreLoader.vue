7e6361aef8533ee379cbecc77ba9c56fbc72ae6f
LucSil <luciansilva351@gmail.com>
2022-06-16 09:56:48 +0100

    created preloader component

---
+++ src/components/PreLoader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="preloader flex justify-center items-center fixed inset-0 w-screen h-screen overflow-hidden bg-gray-600 z-50"
+  >
+    <div class="cube"></div>
+    <div class="cube"></div>
+    <div class="cube"></div>
+    <div class="cube"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PreLoader",
+};
+</script>
+
+<style></style>
