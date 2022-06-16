fd239d3d177a614a19bb6a66fb96eeddbcf7550c
LucSil <luciansilva351@gmail.com>
2022-06-16 18:42:10 +0100

    templates, styles, and logic for PreLoader.vue complete

--- src/components/PreLoader.vue
+++ src/components/PreLoader.vue
@@ -1,18 +1,51 @@
 <template>
   <div
-    class="preloader flex justify-center items-center fixed inset-0 w-screen h-screen overflow-hidden bg-gray-600 z-50"
+    v-if="!loaded"
+    class="preloader grid place-items-center fixed inset-0 w-screen h-screen overflow-y-hidden bg-white z-50"
   >
-    <div class="cube"></div>
-    <div class="cube"></div>
-    <div class="cube"></div>
-    <div class="cube"></div>
+    <div class="allCube relative grid place-items-center w-44 h-44">
+      <span class="text-8xl">🐙</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "PreLoader",
+  data() {
+    return {
+      loaded: false,
+    };
+  },
+  mounted() {
+    document.onreadystatechange = () => {
+      if (document.readyState == "complete") {
+        this.loaded = true;
+      }
+    };
+  },
 };
 </script>
 
-<style></style>
+<style>
+.allCube {
+  animation: y1 3s infinite;
+}
+@keyframes y1 {
+  0% {
+    transform: scale(0.8);
+  }
+  25% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(0.8);
+  }
+  75% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(0.8);
+  }
+}
+</style>
