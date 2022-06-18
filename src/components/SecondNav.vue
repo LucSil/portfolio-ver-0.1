73470f2dd0e53c95a3a88b7fdaa47c4a8f2f6c37
LucSil <luciansilva351@gmail.com>
2022-06-18 22:38:11 +0100

    link animation updated

--- src/components/SecondNav.vue
+++ src/components/SecondNav.vue
@@ -1,6 +1,6 @@
 <template>
   <ul
-    class="secMenu absolute sm:static z-20 grid font-roboto font-light top-36 sm:top-0 tracking-widest text-10-b text-3xl pl-6"
+    class="secMenu absolute sm:static z-20 grid font-roboto font-medium top-36 sm:top-0 tracking-widest text-10-b text-3xl pl-6"
   >
     <router-link @click="close" class="link" to="/">Home</router-link>
     <router-link @click="close" class="link" to="/about">About</router-link>
@@ -17,6 +17,6 @@ export default { name: "SecondNav" };
 .secMenu .router-link-active {
   opacity: 1;
   margin-left: 1rem;
-  transition: all 200 ease-in-out;
+  transition: all 0.6s ease-in-out;
 }
 </style>
