ed9864ad05e06797661c002ef6318a83a92e36b3
LucSil <luciansilva351@gmail.com>
2022-06-18 22:36:12 +0100

    responsive design for min-width:640px

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,37 +1,41 @@
 <template>
-  <section class="grid place-items-center mt-24 relative">
-    <div class="mx-4 mb-12">
-      <h1 class="font-raleway text-10-a text-4xl text-center mb-6">
-        Get In Touch
-      </h1>
-      <p
-        class="text-center text-base font-roboto font-normal tracking-wider leading-6 text-30%"
-      >
-        Hello, once again!<br />I'm currently seeking a new opportunity. If
-        you'd love to hire me, leave me an inbox or a message on any of my
-        social media accounts.
-      </p>
-      <p
-        class="mt-2 text-base text-center font-roboto font-normal tracking-wider leading-6 text-30%"
-      >
-        If you'd also want to have a brief chat, feel free to leave a message!
-      </p>
-    </div>
-    <div class="group">
-      <button
-        class="border-2 border-10-b rounded-md px-6 py-3 group-hover:bg-10-b transition duration-300 ease-linear"
-      >
-        <a
-          class="text-base font-roboto font-medium text-10-b tracking-wider group-hover:text-60%"
-          href="mailto:chijiokeobioha351@gmail.com"
-          rel="noopener noreferrer"
-          target="blank"
-          >Say Hello</a
+  <section class="page grid place-items-center mt-24 relative sm:static">
+    <div class="main grid place-items-center sm:mt-10">
+      <div class="px-4 sm:px-10 mb-12">
+        <h1 class="font-raleway text-10-a text-4xl text-center mb-6">
+          Get In Touch
+        </h1>
+        <p
+          class="text-center text-base font-roboto font-normal tracking-wider leading-6 text-30%"
         >
-      </button>
+          Hello, once again!<br />I'm currently seeking a new opportunity. If
+          you'd love to hire me, leave me an inbox or a message on any of my
+          social media accounts.
+        </p>
+        <p
+          class="mt-2 text-base text-center font-roboto font-normal tracking-wider leading-6 text-30%"
+        >
+          If you'd also want to have a brief chat, feel free to leave a message!
+        </p>
+      </div>
+
+      <!-- Button -->
+      <div class="group">
+        <button
+          class="border-2 border-10-b rounded-md px-6 py-3 group-hover:bg-10-b transition duration-300 ease-linear"
+        >
+          <a
+            class="text-base font-roboto font-medium text-10-b tracking-wider group-hover:text-60%"
+            href="mailto:chijiokeobioha351@gmail.com"
+            rel="noopener noreferrer"
+            target="blank"
+            >Say Hello</a
+          >
+        </button>
+      </div>
+      <ContactSocial class="mt-8 flex justify-between items-center w-[50%]" />
+      <Footer class="sm:hidden" />
     </div>
-    <ContactSocial class="mt-8 flex justify-between items-center w-[50%]" />
-    <Footer />
   </section>
 </template>
 
@@ -43,3 +47,15 @@ export default {
   components: { ContactSocial, Footer },
 };
 </script>
+
+<style>
+@media (min-width: 640px) {
+  .page {
+    display: grid;
+    grid-template-columns: 30vw auto;
+  }
+  .main {
+    grid-column-start: 2;
+  }
+}
+</style>
