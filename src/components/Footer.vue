95c09b69ad49009a690edc146034bf9c0837273d
LucSil <luciansilva351@gmail.com>
2022-06-18 22:37:25 +0100

    minor css additions

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="fixed bottom-0">
+  <footer class="fixed bottom-0 mb-2">
     <p class="py-6 text-xs text-10-b opacity-60">
       <span class="text-10-a">Designed & Built by</span> <em>Destiny Obioha</em>
     </p>
